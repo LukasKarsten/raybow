03aae837dcf808d0d18232550f93e0ecd7604818
Lukas Karsten <lukaskarsten@protonmail.com>
2022-12-07 14:51:37 +0100

    Further optimize intersection calculation

--- src/geometry/sphere.rs
+++ src/geometry/sphere.rs
@@ -1,4 +1,4 @@
-use std::{mem::MaybeUninit, ops::Range, sync::Arc};
+use std::{ops::Range, sync::Arc};
 
 use crate::{material::Material, ray::Ray, vector::Vector};
 
@@ -21,20 +21,15 @@ impl Sphere {
     }
 }
 
-#[repr(C, align(32))]
-struct F64x4([f64; 4]);
-
-#[repr(C, align(16))]
-struct F64Aligned16(f64);
-
 impl Geometry for Sphere {
     fn hit(&self, ray: Ray, t_range: Range<f64>) -> Option<Hit> {
         use std::arch::x86_64::*;
 
-        let radius_squared = self.radius * self.radius;
+        #[repr(C, align(16))]
+        struct Align16<T>(T);
 
-        let a_c_halfb_halfb_vector = MaybeUninit::<F64x4>::uninit();
-        let discriminant = MaybeUninit::<F64Aligned16>::uninit();
+        let halfb_halfb_c_a_vector;
+        let mut discriminant = Align16(0.0);
 
         unsafe {
             let origin = ray.origin.to_simd();
@@ -43,66 +38,57 @@ impl Geometry for Sphere {
 
             let oc = _mm256_sub_pd(origin, center);
 
-            ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-            // dot(velocity, velocity)
-            // dot(oc, velocity)
-            // dot(oc, oc)
             let vel_vel = _mm256_mul_pd(velocity, velocity);
             let oc_oc = _mm256_mul_pd(oc, oc);
             let oc_vel = _mm256_mul_pd(oc, velocity);
 
-            // lsum(vel*vel) | lsum(oc*oc) | hsum(vel*vel) | hsum(oc*oc)
-            // ~~hsum(oc*oc) | hsum(vel*vel) | lsum(oc*oc) | lsum(vel*vel)~~
+            // vel_x^2 + vel_y^2 | oc_x^2 + oc_y^2 | vel_z^2 + vel_w^2 | oc_z^2 + oc_w^2
+            // lsum(vel^2) | lsum(oc^2) | hsum(vel^2) | hsum(oc^2)
             let hadd_vel_vel_oc_oc = _mm256_hadd_pd(vel_vel, oc_oc);
+            // oc_x * vel_x + oc-y * vel_y | oc_x * vel_x + oc-y * vel_y | oc_z * vel_z + oc_w * vel_w | oc_z * vel_z + oc_w * vel_w
             // lsum(oc*vel) | lsum(oc*vel) | hsum(oc*vel) | hsum(oc*vel)
-            // ~~hsum(oc*vel) | hsum(oc*vel) | lsum(oc*vel) | lsum(oc*vel)~~
             let hadd_oc_vel = _mm256_hadd_pd(oc_vel, oc_vel);
 
-            // hsum(oc*vel) | hsum(oc*vel) | lsum(vel*vel) | lsum(oc*oc)
-            // ~~lsum(oc*vel) | lsum(oc*vel) | hsum(oc*oc) | hsum(vel*vel)~~
+            // lsum(oc*vel) | lsum(oc*vel) | hsum(oc^2) | hsum(vel^2)
             let swapped = _mm256_permute2f128_pd::<0x21>(hadd_vel_vel_oc_oc, hadd_oc_vel);
-            // lsum(oc*vel) | lsum(oc*vel) | hsum(vel*vel) | hsum(oc*oc)
-            // ~~hsum(oc*vel) | hsum(oc*vel) | lsum(oc*oc) | lsum(vel*vel)~~
+            // hsum(oc*vel) | hsum(oc*vel) | lsum(oc^2) | lsum(vel^2)
             let blended = _mm256_blend_pd::<0b1100>(hadd_vel_vel_oc_oc, hadd_oc_vel);
 
-            // oc . vel | oc . vel | oc . oc | vel . vel
-            // oc . oc | vel . vel | oc . vel | oc . vel
-            let sum = _mm256_add_pd(swapped, blended);
+            // halfb | halfb | |oc|^2 | a
+            let dots = _mm256_add_pd(swapped, blended);
 
-            ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+            let radius = _mm256_set_pd(0.0, 0.0, self.radius, 0.0);
+            let halfb_halfb_c_a = _mm256_fnmadd_pd(radius, radius, dots);
+            let halfb_halfb = _mm256_extractf128_pd::<1>(halfb_halfb_c_a);
 
-            let radius = _mm256_set_pd(0.0, 0.0, radius_squared, 0.0);
+            halfb_halfb_c_a_vector = Vector::from_simd(halfb_halfb_c_a);
 
-            let a_c_halfb_halfb = _mm256_sub_pd(sum, radius);
+            let halfb_halfb_a_c = _mm256_permute_pd(halfb_halfb_c_a, 0b1001);
 
-            _mm256_store_pd(a_c_halfb_halfb_vector.as_ptr() as _, a_c_halfb_halfb);
+            let halfbsqr_halfbsqr_ac_ac = _mm256_mul_pd(halfb_halfb_c_a, halfb_halfb_a_c);
+            let halfbsqr_halfbsqr = _mm_mul_pd(halfb_halfb, halfb_halfb);
 
-            let c_a_halfb_halfb = _mm256_permute_pd(a_c_halfb_halfb, 0b1001);
-
-            // ac | ac | halfb^2 | halfb^2
-            let tmp1 = _mm256_mul_pd(a_c_halfb_halfb, c_a_halfb_halfb);
-            // halfb^2 | halfb^2
-            let tmp2 = _mm256_extractf128_pd::<1>(tmp1);
-            // halfb^2 - ac | halfb^2 - ac
-            let result = _mm_sub_pd(tmp2, _mm256_castpd256_pd128(tmp1));
-
-            _mm_store_pd1(discriminant.as_ptr() as _, result);
+            let discriminants = _mm_sub_pd(
+                halfbsqr_halfbsqr,
+                _mm256_castpd256_pd128(halfbsqr_halfbsqr_ac_ac),
+            );
+            _mm_store_pd1(&mut discriminant.0, discriminants);
         }
 
-        let [a, _, half_b, _] = unsafe { a_c_halfb_halfb_vector.assume_init() }.0;
-        let discriminant = unsafe { discriminant.assume_init().0 };
+        let [a, _, half_b, _] = halfb_halfb_c_a_vector.0;
+        let discriminant = discriminant.0;
 
         /*
         {
             let _oc = ray.origin - self.center;
-            let _a = ray.velocity.magnitude_squared();
-            let _half_b = _oc.dot(&ray.velocity);
-            let _c = _oc.magnitude_squared() - self.radius * self.radius;
+            let _a = ray.velocity.length_squared();
+            let _half_b = _oc.dot(ray.velocity);
+            let _c = _oc.length_squared() - self.radius * self.radius;
             let _discriminant = _half_b * _half_b - _a * _c;
 
-            assert_eq!(half_b, _half_b);
             assert_eq!(a, _a);
+            assert_eq!(half_b, _half_b);
+            assert_eq!(c, _c);
             assert_eq!(discriminant, _discriminant);
         }
         */
