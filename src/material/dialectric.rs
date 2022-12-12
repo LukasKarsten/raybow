7d3a4357f759daa1be1c56ce1c35d317ac42d6de
Lukas Karsten <lukaskarsten@protonmail.com>
2022-12-12 14:47:47 +0100

    Move function out of impl block

--- src/material/dialectric.rs
+++ src/material/dialectric.rs
@@ -8,14 +8,6 @@ pub struct Dialectric {
     pub index: f64,
 }
 
-impl Dialectric {
-    fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
-        let r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
-        let r0 = r0 * r0;
-        r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
-    }
-}
-
 impl Material for Dialectric {
     fn scatter(&self, hit: &Hit) -> Option<(Ray, Color)> {
         let attenuation = Color::from_rgb(1.0, 1.0, 1.0);
@@ -31,7 +23,7 @@ impl Material for Dialectric {
         let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
 
         let can_refract = refraction_ratio * sin_theta > 1.0
-            || Self::reflectance(cos_theta, refraction_ratio) > rand::thread_rng().gen();
+            || reflectance(cos_theta, refraction_ratio) > rand::thread_rng().gen();
 
         let dir = if can_refract {
             reflect(unit_vel, hit.normal)
@@ -41,4 +33,10 @@ impl Material for Dialectric {
 
         Some((Ray::new(hit.point, dir), attenuation))
     }
+}
+
+fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
+    let r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+    let r0 = r0 * r0;
+    r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
 }
